33e406f121ad03a51cff180ae69e88b0ebcb9678
HeeHooHo <sabrinatruth@tutanota.com>
2022-02-05 19:42:30 +0000

    Bro can I put this here?
    Dude check it out.

---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    CharacterController characterController;
+
+    public float speed=5;
+    private Vector3 moveDirection = Vector3.zero;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+        moveDirection *= speed;
+        characterController.Move(moveDirection*Time.deltaTime);
+        /*float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+        gameObject.transform.Translate(new Vector3(x,0,z)* speed *Time.deltaTime);*/
+    }
+}
---
+++ Assets/Scripts/playerShoot.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerShoot : MonoBehaviour
+{
+    
+    public Transform GlassEm;
+    public GameObject Glass;
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.I))
+        {
+            gameObject.transform.Rotate(new Vector3(0,180,0));
+            Shoot();
+        }
+        if(Input.GetKeyDown(KeyCode.J))
+        {
+            gameObject.transform.Rotate(new Vector3(0,90,0));
+            Shoot();
+        }
+        if(Input.GetKeyDown(KeyCode.L))
+        {
+            gameObject.transform.Rotate(new Vector3(0,270,0));
+            Shoot();
+        }
+
+        if(Input.GetKeyUp(KeyCode.K))
+        {
+            transform.rotation =Quaternion.Euler(0,90, 0);
+            Shoot();
+        }
+        if(Input.GetKeyUp(KeyCode.I)||Input.GetKeyUp(KeyCode.J)||Input.GetKeyUp(KeyCode.L))
+        {
+           
+            transform.rotation =Quaternion.Euler(0,90, 0);
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(Glass, GlassEm.position, GlassEm.rotation);
+    }
+}
